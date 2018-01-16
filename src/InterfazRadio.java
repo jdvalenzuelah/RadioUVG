ac2557f6367fb559d7ce2ff3b3a851241dc32db2
daval <daval@DESKTOP-N9BGAO8>
2018-01-16 15:24:24 -0600

    Implementacion

--- src/InterfazRadio.java
+++ src/InterfazRadio.java
@@ -7,13 +7,53 @@
  *
  */
 public interface InterfazRadio {
-
+	/**
+	 * 
+	 * @return
+	 */
 	public String frecAdelante();
+	
+	/**
+	 * 
+	 * @return
+	 */
 	public String frecAtras();
+	
+	/**
+	 * 
+	 * @return
+	 */
 	public boolean prender();
+	
+	/**
+	 * 
+	 * @return
+	 */
 	public boolean apagar();
+	
+	/**
+	 * 
+	 * @return
+	 */
 	public String cambioFrecuencia();
+	
+	/**
+	 * 
+	 * @param frec
+	 * @param pos
+	 */
 	public void guardarFrecAm(int frec, int pos);
+	
+	/**
+	 * 
+	 * @param frec
+	 * @param pos
+	 */
 	public void guardarFrecFm(float frec, int pos);
+	
+	/**
+	 * 
+	 * @return
+	 */
 	public String mostrarEstacion(); 
 }
--- src/Radio.java
+++ src/Radio.java
@@ -8,28 +8,72 @@
  */
 public class Radio implements InterfazRadio {
 
+	private boolean power;
+	private String frecueciaActual;
+	private String estacionActual;
+	
+	/**
+	 * 
+	 */
+	public Radio() {
+		this.frecueciaActual = "am";
+		this.estacionActual = "530";
+		this.power = false;
+	}
+	
 	@Override
 	public String frecAdelante() {
-		// TODO Auto-generated method stub
-		return null;
+		switch(this.getFrecueciaActual().toLowerCase()) {
+		case "am":
+			if(Integer.parseInt(this.estacionActual)  < 1610) {
+				this.estacionActual=  Integer.toString((Integer.parseInt(this.mostrarEstacion()) + 10));
+			}else {
+				this.estacionActual = Integer.toString(530);
+			}
+			break;
+		case "fm":
+			if(Float.parseFloat(this.estacionActual) < 107.9) {
+				this.estacionActual = Float.toString(Float.parseFloat(this.mostrarEstacion()) + (float) 0.2);
+			}else {
+				this.estacionActual = Float.toString((float) 87.9);
+			}
+			break;
+		}
+		return this.estacionActual;
 	}
+
 
 	@Override
 	public String frecAtras() {
-		// TODO Auto-generated method stub
-		return null;
+		switch(this.getFrecueciaActual().toLowerCase()) {
+		case "am":
+			if(Integer.parseInt(this.estacionActual)  > 530) {
+				this.estacionActual=  Integer.toString((Integer.parseInt(this.mostrarEstacion()) - 10));
+			}else {
+				this.estacionActual = Integer.toString(1610);
+			}
+			break;
+		case "fm":
+			if(Float.parseFloat(this.estacionActual) > 87.9) {
+				this.estacionActual = Float.toString(Float.parseFloat(this.mostrarEstacion()) - (float) 0.2);
+			}else {
+				this.estacionActual = Float.toString((float) 107.9);
+			}
+			break;
+		}
+		return this.estacionActual;
 	}
 
 	@Override
 	public boolean prender() {
-		// TODO Auto-generated method stub
-		return false;
+		this.setPower(true);
+		return true;
 	}
 
 	@Override
 	public boolean apagar() {
-		// TODO Auto-generated method stub
-		return false;
+		this.setPower(false);
+		return true;
 	}
 
 	@Override
@@ -52,8 +96,36 @@ public class Radio implements InterfazRadio {
 
 	@Override
 	public String mostrarEstacion() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return this.estacionActual;
+	}
+
+	/**
+	 * @return the power
+	 */
+	public boolean isPower() {
+		return power;
+	}
+
+	/**
+	 * @param power the power to set
+	 */
+	public void setPower(boolean power) {
+		this.power = power;
+	}
+
+	/**
+	 * @return the frecueciaActual
+	 */
+	public String getFrecueciaActual() {
+		return frecueciaActual;
+	}
+
+	/**
+	 * @param frecueciaActual the frecueciaActual to set
+	 */
+	public void setFrecueciaActual(String frecueciaActual) {
+		this.frecueciaActual = frecueciaActual;
 	}
 
 }
