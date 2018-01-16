1740a6b8492de7483d67afe2b038850b37c2ffd5
daval <daval@DESKTOP-N9BGAO8>
2018-01-16 10:02:29 -0600

    ADT & DE

--- src/InterfazRadio.java
+++ src/InterfazRadio.java
@@ -8,4 +8,12 @@
  */
 public interface InterfazRadio {
 
+	public String frecAdelante();
+	public String frecAtras();
+	public boolean prender();
+	public boolean apagar();
+	public String cambioFrecuencia();
+	public void guardarFrecAm(int frec, int pos);
+	public void guardarFrecFm(float frec, int pos);
+	public String mostrarEstacion(); 
 }
---
+++ src/Radio.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+
+/**
+ * @author daval
+ *
+ */
+public class Radio implements InterfazRadio {
+
+	@Override
+	public String frecAdelante() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String frecAtras() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean prender() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean apagar() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String cambioFrecuencia() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void guardarFrecAm(int frec, int pos) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void guardarFrecFm(float frec, int pos) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String mostrarEstacion() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
