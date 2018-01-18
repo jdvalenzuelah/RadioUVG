974a1dc824e6ba11ee11b1aa0d35a3c63ce9edcf
Josue David Valenzuela Herrera <val171001@uvg.edu.gt>
2018-01-18 15:46:16 -0600

    - Creacion Pruebas JUnit

---
+++ src/PruebaRadio.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PruebaRadio {
+
+	@Before
+	public void before() {
+		RadioI radio = new Radio();
+	}
+	
+	@Test
+	public void prueba() {
+		
+	}
+	
+}
