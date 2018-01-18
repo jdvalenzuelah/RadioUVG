de15babc037da083a99428f6aeaf378e2404d6ab
Josue David Valenzuela Herrera <val171001@uvg.edu.gt>
2018-01-18 13:00:02 -0600

    Update

--- src/InterfazRadio.java
+++ src/InterfazRadio.java
@@ -7,66 +7,7 @@
  *
  */
 public interface InterfazRadio {
-	/**
-	 * 
-	 * @return
-	 */
-	public String frecAdelante();
+
 	
-	/**
-	 * 
-	 * @return
-	 */
-	public String frecAtras();
-	
-	/**
-	 * 
-	 * @return
-	 */
-	public boolean prender();
-	
-	/**
-	 * 
-	 * @return
-	 */
-	public boolean apagar();
-	
-	/**
-	 * 
-	 * @return
-	 */
-	public String cambioFrecuencia();
-	
-	/**
-	 * 
-	 * @param frec
-	 * @param pos
-	 */
-	public void guardarFrecAm(int frec, int pos);
-	
-	/**
-	 * 
-	 * @param frec
-	 * @param pos
-	 */
-	public void guardarFrecFm(float frec, int pos);
-	
-	/**
-	 * 
-	 * @return
-	 */
-	public String mostrarEstacion(); 
-	
-	/**
-	 * 
-	 * @return
-	 */
-	public boolean estaPrendido();
-	
-	/**
-	 * 
-	 * @return
-	 */
-	public String obtenerEstacion(int pos);
 	
 }
